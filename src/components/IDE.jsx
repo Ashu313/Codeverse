c7a2cea8a988ebd61df481ad79dac3b58dbb573c
Ashu313 <79858134+Ashu313@users.noreply.github.com>
2023-09-25 12:38:12 +0530

    werbsocket for peerjs

--- src/components/IDE.jsx
+++ src/components/IDE.jsx
@@ -74,23 +74,7 @@ export default function IDE({ docId, modal, toggleModal, cpp14, setcpp14, java, 
     peer?.on('error', (error) => {
       console.error('PeerJS error:', error);
     });
-    socket?.emit('join-room', 'room123', 'user123', 'Alice');
-
-    // Example: Sending a toggle event
-    socket?.emit('toggled', 'user123', true, false);
-
-    // Listening for events from the server
-    socket?.on('user-connected', (userId, userName) => {
-      console.log(`User ${userName} (${userId}) connected.`);
-    });
-
-    socket?.on('received-toggled-events', (userId, video, audio) => {
-      console.log(`Received toggled event from ${userId}: Video: ${video}, Audio: ${audio}`);
-    });
-
-    socket?.on('user-disconnected', (userId) => {
-      console.log(`User ${userId} disconnected.`);
-    });
+   
 
     useEffect(() => {
         if (socket == null) return;
